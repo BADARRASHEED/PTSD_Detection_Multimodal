926509e8a362ed9cb2453a0f5e7355030c9da7ae
Badar Butt <211370113@gift.edu.pk>
2025-07-21 14:22:41 +0500

    Corrected

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client"; // Marking this file as a client-side component
+
 import { useState } from "react";
 import { useRouter } from "next/navigation"; // useRouter for navigation
 import Link from "next/link";
